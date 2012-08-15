8f67616e3bf5e83aecc9bc05757044b9657a851f
Gerard Devine <g.m.devine@reading.ac.uk>
2012-08-15 11:21:15 +0100

    setting up fabfile.py

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+from fabric.api import *
+from os import path
+
+
+env.user = 'root'
+env.shell = "/bin/bash -c"
+env.hosts = ['server_ip',] # these are defined in your /.ssh/config Host host1, Host host2, etc.
+env.password = "" # at lease for me it's easier to keep my password in the fabfile
+
+
+### Local tasks
+def commit(message):
+local('git add .') 
+_commit = "git commit -a -m '{0}'".format(message)
+local(_commit)
+local('git push origin master')
+
+
+### Server tasks
+def reload_server():
+    sudo('invoke-rc.d apache2 reload')
+
+
+def deploy():
+    with cd('/home/djangoapp'):
+        sudo('git pull')
+    
+    reload_server
