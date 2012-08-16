d9a13507213fc23033fbd01d24819eedf7483fc4
Gerard Devine <g.m.devine@reading.ac.uk>
2012-08-16 10:58:17 +0100

    command line bug edited

--- fabfile.py
+++ fabfile.py
@@ -41,4 +41,4 @@ def prepare_deployment():
     local('git add .')
     local('git commit') 
     local('git push origin master')  # Push master to github repository
-    local('echo "Deployment success')
+    local('echo "Deployment success"')
