fbefaecf13e61011aa6a4766bccff15f0eafe540
Gerard Devine <g.m.devine@reading.ac.uk>
2012-10-05 15:59:51 +0100

    tooltips helpers added

--- cimexpgen/apps/expgenapp/forms.py
+++ cimexpgen/apps/expgenapp/forms.py
@@ -16,8 +16,7 @@ class ExperimentForm(ModelForm):
         model = Experiment 
         widgets = {
             'requirements': CheckboxSelectMultiple(),
-        } 
-        #requirements = forms.ModelMultipleChoiceField(queryset=NumericalRequirement.objects.all())
+        }        
         
         
 class RequirementForm(ModelForm):
@@ -28,6 +27,16 @@ class RequirementForm(ModelForm):
     
     reqtype = forms.CharField(widget=forms.Select(choices=CHOICES, attrs={'size':'1'}))
 
+
     class Meta:
-        model = NumericalRequirement 
+        model = NumericalRequirement
+        
+        
+    def clean_reqtype(self):
+        '''Check that an actual valid choice is selected'''
+        reqtype = self.cleaned_data['reqtype']
+        if reqtype == u'--------------':
+            raise forms.ValidationError("This field is required")
+          
+        return reqtype 
         
